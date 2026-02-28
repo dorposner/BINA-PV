33618a9a43923f96d4b2d83a4c3e9d589fbfecbe
Dor Posner <dorposner@gmail.com>
2026-02-28 15:34:11 -0600

    Migrate from Decap to Sveltia CMS and update image schema paths to public

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,21 +2,21 @@ import { z, defineCollection } from 'astro:content';
 
 const eventsCollection = defineCollection({
     type: 'content',
-    schema: ({ image }) => z.object({
+    schema: z.object({
         title: z.string(),
         date: z.date(),
         time: z.string().optional(),
         description: z.string().optional(),
-        image: image().optional(),
+        image: z.string().optional(),
         registrationLink: z.string().url().optional().or(z.literal('')),
     }),
 });
 
 const recommendationsCollection = defineCollection({
     type: 'content',
-    schema: ({ image }) => z.object({
+    schema: z.object({
         title: z.string(),
-        imageGallery: z.array(image()).optional(),
+        imageGallery: z.array(z.string()).optional(),
         googleMapsEmbed: z.string().optional(),
         externalLink: z.string().url().optional().or(z.literal('')),
     }),
