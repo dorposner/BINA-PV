cc22e4d78b004a2d241ed2a75caec5573a6670a0
Dor Posner <dorposner@gmail.com>
2026-05-07 10:45:28 -0600

    Add Tailwind integration to support game page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,20 @@ import sitemap from '@astrojs/sitemap';
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bina.kesher.me',
-    integrations: [sitemap(), react()],
-    i18n: {
-        defaultLocale: "he",
-        locales: ["he", "en"],
-        routing: { prefixDefaultLocale: false }
-    }
+  site: 'https://bina.kesher.me',
+  integrations: [sitemap(), react()],
+
+  i18n: {
+      defaultLocale: "he",
+      locales: ["he", "en"],
+      routing: { prefixDefaultLocale: false }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
